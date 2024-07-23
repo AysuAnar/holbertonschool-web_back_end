9852d67644a94b6d304b46cc2ec8b9dcabc40fbe
AysuAnar <8921@holbertonschoolstudents.com>
2024-07-23 16:20:49 +0400

    task1

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,8 @@
-function getListStudents() {
-    return [
+export default function getListStudents() {
+    const student = [
         { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
         { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+        { id: 5, firstName: 'Serena', location: 'San Francisco' },
     ];
+    return student;
 }
-
-// Example usage:
-const students = getListStudents();
-console.log(students);
